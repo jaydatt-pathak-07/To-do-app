8ae3fbaef76977acb627d483d3d8fb8b17b79872
jaydatt-pathak-07 <pjaydatt58@gmail.com>
2022-03-29 22:05:01 +0530

    settings updated

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'msb-0s4&&ebf&ajzn^e=4!6#p!mh^!uz7=cv%qf1smnaoe7l$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['calm-savannah-19873.herokuapp.com']
 
