651a80e41a8b74ed53a3b105a63f462e5fe16230
jaydatt-pathak-07 <pjaydatt58@gmail.com>
2022-03-29 21:33:23 +0530

    settings updated

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -148,9 +148,13 @@ if DEBUG:
 
 
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
     
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
