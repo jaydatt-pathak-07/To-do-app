8cb11c7abcca376eddea5793c6f3d5905f40fb15
jaydatt-pathak-07 <pjaydatt58@gmail.com>
2022-03-29 20:59:47 +0530

    postgresql updated

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'todoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER' : 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost'
     }
 }
 
